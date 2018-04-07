314d1a87c9a0d343d537c10c5789a6b42342dae6
= <kj.tokarz@gmail.com>
2018-04-07 17:24:27 -0400

    Move from random project files to sln

---
+++ SimpleEncryptedChatClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleEncryptedChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var message = String.Empty;
+			Console.WriteLine("Enter a message to send");
+			while (message != "q")
+			{
+				message = Console.ReadLine();
+				using (var client = new UdpClient())
+				{
+					client.Connect(IPAddress.Parse("127.0.0.1"), 9000);
+					Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+					client.Send(data, data.Length);
+				}
+			}
+		}
+    }
+}
---
+++ SimpleEncryptedChatServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleEncryptedChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Server Starting");
+			while (true)
+			{
+				using (var client = new UdpClient(9000))
+				{
+					var ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+					Byte[] receivedBytes = client.Receive(ref ipEndPoint);
+					string message = System.Text.Encoding.ASCII.GetString(receivedBytes);
+					Console.WriteLine($"Message received: {message}");
+				}
+			}
+		}
+    }
+}
